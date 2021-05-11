c811bd1f8b590487e562ce1139ed18cd61db02f3
DefinitelyRus <definitelyrus@gmail.com>
2021-05-11 14:36:53 +0800

    Cleaned pointSystem module

--- src/commands/pointSystem.java
+++ src/commands/pointSystem.java
@@ -1,11 +1,7 @@
 package commands;
 
-
-
 import java.sql.SQLException;
-
 import javax.security.auth.login.LoginException;
-
 import bot_init.LazyJavie;
 import bot_init.SQLconnector;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -17,22 +13,18 @@ public class pointSystem extends ListenerAdapter {
 	
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		
-		
-
-		//on message detect
 		try {
 			//grabs the message and get the (discord id) of the member who sent the message
 			Message message = event.getMessage();
 			String memberId = message.getMember().getId();
 			//gets the member's amount of points (database)
-			String stringPoints = SQLconnector.get("select points from lazyjavie.members WHERE userid=" + memberId + ";", "points");	
-			Integer integerPoints = Integer.parseInt(stringPoints);
-			integerPoints += 1;
+			Integer pts = Integer.parseInt(SQLconnector.get("select points from lazyjavie.members WHERE userid=" + memberId + ";", "points"));
+			pts += 1;
 			//updates the number of points to the new one
-			SQLconnector.update("UPDATE lazyjavie.members "+ "SET points = " + integerPoints + " WHERE userid=" + memberId + ";");
+			SQLconnector.update("UPDATE lazyjavie.members "+ "SET points = " + pts + " WHERE userid=" + memberId + ";");
 			
 			String[] args = event.getMessage().getContentRaw().split("\\s+");	
-			String requestby = event.getMember().getUser().getName();
+			String memberName = event.getMember().getUser().getName();
 
 			if (args[0].equalsIgnoreCase(LazyJavie.prefix + "points") || args[0].equalsIgnoreCase(LazyJavie.prefix + "point")  ) {
 				if (args.length < 2) {
@@ -40,25 +32,21 @@ public class pointSystem extends ListenerAdapter {
 					EmbedBuilder points = new EmbedBuilder();
 					points.setColor(0xffae00);
 					points.setTitle("💲 Your current points: 💲");
-					points.setDescription("" + integerPoints + "");
-					
-
-					
-					
-					points.setFooter("Requested by " + requestby , event.getMember().getUser().getAvatarUrl());
+					points.setDescription("" + pts + "");
+					points.setFooter("Requested by " + memberName , event.getMember().getUser().getAvatarUrl());
 					event.getChannel().sendMessage(points.build()).queue();
 				}
 			}
-
 		} catch (LoginException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		catch (NumberFormatException e) {
-				if (e.toString().startsWith("java.lang.NumberFormatException: For input string: \"Statement created.\"")) {
-					P.print("SQL Statement created");
-				}
+		} catch (NumberFormatException e) {
+			if (e.toString().startsWith("java.lang.NumberFormatException: For input string: \"Statement created.\"")) {
+				P.print("[pointSystem] SQL Statement created. (False error)");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
