9097ac8616d75dd5780556d385ea7213b44612ed
Rus <definitelyrus@gmail.com>
2021-08-25 13:30:28 +0800

    Added pre-startup sequence

--- src/home/ConsoleCallables.java
+++ src/home/ConsoleCallables.java
@@ -3,39 +3,58 @@ package home;
 import javax.annotation.Nullable;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+
 import commands.P;
 import commands.Quit;
 
 public class ConsoleCallables {
 	
 	public static void main(String args[]) {
-		startBot("Start", null);
+		String tokenOverride = null;
+		P.print("[LazyJavie] Pre-startup sequence...");
+		P.print("|Looking for LazyJavie.db...");
+		if (SQLconnector.dbCheck() == false) {
+			P.print("|Attempting to create a new database...");
+			SQLconnector.NoDBfixer();
+		}
+		
+		//Attempts to get a replacement token saved from database.
+		tokenOverride = SQLconnector.get("select * from botsettings where name = 'discord_bot_token_override'", "value", false);
+		P.print(tokenOverride);
+		
+		//Prompts the bot for startup with the assigned token.
+		P.print("|Attempting to start LazyJavie...");
+		//startBot("Start", tokenOverride);
 	}
 	
-	public static void startBot(String taskName, @Nullable String botTokenOverride) {
-		if (taskName.equalsIgnoreCase("Start")) {
+	public static void startBot(String task, @Nullable String botTokenOverride) {
+		if (task.equalsIgnoreCase("Start")) {
 			Bot.isAwake = true;
 			
 			//Determines whether the token should be grabbed from the UI or from system.
-			if (botTokenOverride.equals("") || botTokenOverride.equals(null)) {Bot.tokenOverride = false;}
+			if (botTokenOverride == null) {Bot.tokenOverride = false;}
 			else {Bot.tokenOverride = true; Bot.token = botTokenOverride;}
 			
-			//Finally starts the bot.
-			Bot.start();
+			/* Finally starts the bot.
+			 * 
+			 * Bot.start() returns a boolean value which corresponds to whether
+			 * it was successful in starting up. Whenever it returns false,
+			 * the following line will close the program automatically.
+			 */
+			P.print("Awaiting startup...");
+			boolean status = Bot.start();
+			if (status == false) return;
 			
 			//Waits until the bot is ready. (Otherwise, the code will continue but the cache isn't yet ready, causing errors.)
 			try {Bot.jda.awaitReady();}
 			catch (InterruptedException e) {SQLconnector.callError(e.toString(), ExceptionUtils.getStackTrace(e)); P.print(e.toString());}
 			catch (NullPointerException e) {SQLconnector.callError(e.toString() + " - likely caused by bad connection.", ExceptionUtils.getStackTrace(e)); P.print(e.toString());}
 			
-			P.print("Console ready!");
 			return;
 		
-		} else if (taskName.equalsIgnoreCase("Stop")) {
+		} else if (task.equalsIgnoreCase("Stop")) {
 			Bot.isAwake = false;
 			Quit.softExit();
-			
-			//Look for an alternative way to delay changing the text
 			return;
 		}
 	}
