45f108a8c634845dae87e84fc880694f9a0e1297
Rus <definitelyrus@gmail.com>
2021-08-26 11:22:35 +0800

    Added use for send message shortcut.

--- src/commands/Quit.java
+++ src/commands/Quit.java
@@ -1,16 +1,19 @@
-/*
- * Lmao
- */
 package commands;
 
 import java.util.concurrent.TimeUnit;
 
 import home.Bot;
+import home.P;
 import home.SQLconnector;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+/**
+ * 
+ * @author DefinitelyRus
+ *
+ */
 public class Quit extends ListenerAdapter{
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		String[] args = event.getMessage().getContentRaw().split("\\s+");
@@ -19,6 +22,7 @@ public class Quit extends ListenerAdapter{
 		try {
 			boolean isAdmin = event.getMember().hasPermission(Permission.ADMINISTRATOR);
 			
+			//Case: Unfinished command.
 			if (args[0].equalsIgnoreCase(Bot.prefix + "quit")) {
 				P.print("\n[quit] Exit request by: " + event.getMember().getUser().getName());
 			}
@@ -26,21 +30,21 @@ public class Quit extends ListenerAdapter{
 			//Case: User is not an admin.
 			if (args[0].equalsIgnoreCase(Bot.prefix + "quit") && isAdmin == false) {
 				P.print("Requester is not an admin; cancelling.");
-				event.getChannel().sendMessage("Only admins can do that!").queue();
+				P.send(event, "Only admins can do that!");
 				return;
 			}
 			
 			//Case: User is an admin and completes the default prompt.
 			else if (args[0].equalsIgnoreCase(Bot.prefix + "quit") && args[1].equalsIgnoreCase("confirm") && isAdmin == true) {
 				P.print("Shutting down LazyJavie 2.0 ALPHA.");
-				event.getChannel().sendMessage("Bye bye!").queue();
+				P.send(event, "Bye bye!");
 				TimeUnit.SECONDS.sleep(5);
 				exit();
 			}
 			
 			//Case: User is an admin and completes the soft-exit prompt.
 			else if (args[0].equalsIgnoreCase(Bot.prefix + "quit") && args[1].equalsIgnoreCase("confirmKeepUI") && isAdmin == true) {
-				event.getChannel().sendMessage("Bye bye!").queue();
+				P.send(event, "Bye bye!");
 				TimeUnit.SECONDS.sleep(5);
 				softExit();
 			}
@@ -48,11 +52,11 @@ public class Quit extends ListenerAdapter{
 			//Case: User is an admin, but doesn't complete the prompt.
 			else if (args[0].equalsIgnoreCase(Bot.prefix + "quit") && isAdmin == true) {
 				P.print("args[1] is not 'confirm'; cancelling.");
-				event.getChannel().sendMessage(args[1] + " is not 'confirm' or 'confirmKeepUI'.").queue();
+				P.send(event, args[1] + " is not 'confirm' or 'confirmKeepUI'.");
 			}
 		} catch (ArrayIndexOutOfBoundsException e) {
 			P.print("Error ignored: Missing args.");
-			event.getChannel().sendMessage(":warning: Are you sure you want to disconnect the bot?\nEnter `" +Bot.prefix+ "quit confirm` to confirm.").queue();
+			P.send(event, ":warning: Are you sure you want to disconnect the bot?\nEnter `" +Bot.prefix+ "quit confirm` to confirm.");
 			SQLconnector.callError(msg, "[QUIT] MISSING ARGS");
 			return;
 		} catch (Exception e) {P.print(e.toString()); return;}
